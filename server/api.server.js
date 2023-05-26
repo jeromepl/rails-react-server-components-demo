ae401daa8ef8acf903dfeb46abfed87da47ed08d
Jérôme Parent-Lévesque <jerome.parent-levesque@potloc.com>
2023-05-26 16:27:37 -0400

    Cleanup express server code

--- server/api.server.js
+++ server/api.server.js
@@ -21,22 +21,10 @@ babelRegister({
 const express = require('express');
 const compress = require('compression');
 const {readFileSync} = require('fs');
-const {unlink, writeFile} = require('fs').promises;
-const {renderToPipeableStream} = require('react-server-dom-webpack/server');
 const path = require('path');
-const {Pool} = require('pg');
-const React = require('react');
-const ReactApp = require('../src/App').default;
-const proxy = require('express-http-proxy');
-
-// Don't keep credentials in the source tree in a real app!
-const pool = new Pool(require('../credentials'));
 
 const PORT = process.env.PORT || 4000;
 const app = express();
-
-app.use('/rails', proxy('http://server:3000'));
-// app.disable('etag');
 
 app.use(compress());
 app.use(express.json());
@@ -89,108 +77,6 @@ app.get(
     res.send(html);
   })
 );
-
-async function renderReactTree(res, props) {
-  await waitForWebpack();
-  const manifest = readFileSync(
-    path.resolve(__dirname, '../build/react-client-manifest.json'),
-    'utf8'
-  );
-  const moduleMap = JSON.parse(manifest);
-  const {pipe} = renderToPipeableStream(
-    React.createElement(ReactApp, props),
-    moduleMap
-  );
-  pipe(res);
-}
-
-function sendResponse(req, res, redirectToId) {
-  const location = JSON.parse(req.query.location);
-  if (redirectToId) {
-    location.selectedId = redirectToId;
-  }
-  res.set('X-Location', JSON.stringify(location));
-  renderReactTree(res, {
-    selectedId: location.selectedId,
-    isEditing: location.isEditing,
-    searchText: location.searchText,
-  });
-}
-
-app.get('/react', function(req, res) {
-  sendResponse(req, res, null);
-});
-
-const NOTES_PATH = path.resolve(__dirname, '../notes');
-
-app.post(
-  '/notes',
-  handleErrors(async function(req, res) {
-    const now = new Date();
-    const result = await pool.query(
-      'insert into notes (title, body, created_at, updated_at) values ($1, $2, $3, $3) returning id',
-      [req.body.title, req.body.body, now]
-    );
-    const insertedId = result.rows[0].id;
-    await writeFile(
-      path.resolve(NOTES_PATH, `${insertedId}.md`),
-      req.body.body,
-      'utf8'
-    );
-    sendResponse(req, res, insertedId);
-  })
-);
-
-app.put(
-  '/notes/:id',
-  handleErrors(async function(req, res) {
-    const now = new Date();
-    const updatedId = Number(req.params.id);
-    await pool.query(
-      'update notes set title = $1, body = $2, updated_at = $3 where id = $4',
-      [req.body.title, req.body.body, now, updatedId]
-    );
-    await writeFile(
-      path.resolve(NOTES_PATH, `${updatedId}.md`),
-      req.body.body,
-      'utf8'
-    );
-    sendResponse(req, res, null);
-  })
-);
-
-app.delete(
-  '/notes/:id',
-  handleErrors(async function(req, res) {
-    await pool.query('delete from notes where id = $1', [req.params.id]);
-    await unlink(path.resolve(NOTES_PATH, `${req.params.id}.md`));
-    sendResponse(req, res, null);
-  })
-);
-
-app.get(
-  '/notes',
-  handleErrors(async function(_req, res) {
-    const {rows} = await pool.query('select * from notes order by id desc');
-    res.json(rows);
-  })
-);
-
-app.get(
-  '/notes/:id',
-  handleErrors(async function(req, res) {
-    const {rows} = await pool.query('select * from notes where id = $1', [
-      req.params.id,
-    ]);
-    res.json(rows[0]);
-  })
-);
-
-app.get('/sleep/:ms', function(req, res) {
-  setTimeout(() => {
-    res.json({ok: true});
-  }, req.params.ms);
-});
 
 app.use(express.static('build'));
 app.use(express.static('public'));
